f5196d173485811cd35ffd018ed93a85cfdd053b
tindat1442-cpu <tindat1442@gmail.com>
2026-07-10 03:32:43 -0400

    complete

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,12 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-        const {name,img,cost} = action.payload;
+        const {name,image,cost} = action.payload;
         const existingItem = state.items.find(item => item.name === name)
         if(existingItem){
-            existingItem.quantity+
+            existingItem.quantity+=1
         } else {
-            state.items.push({name,img,cost,quantity:1})
+            state.items.push({name,image,cost,quantity:1})
         }
     },
     removeItem: (state, action) => {
